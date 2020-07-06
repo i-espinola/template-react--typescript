00b5e7ff4a9f3c20f9f9f8f61820181e95756ff4
i-espinola <ivan.espinola@live.com>
2020-07-06 02:51:35 -0300

    update server and package

---
+++ server/favicon.d.ts
@@ -0,0 +1,3 @@
+import favicon from 'express-favicon';
+
+export default favicon;
--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,13 @@
 // Express server
 import Setup from './setup';
 import http from 'http';
-// import favicon from 'express-favicon';
+import favicon from './favicon';
 import express from 'express';
 import path from 'path';
 
 const app = express();
 
-// app.use(favicon(Setup.path + Setup.favicon));
+app.use(favicon(Setup.path + Setup.favicon));
 app.use(express.static(Setup.path));
 app.use(express.static(path.join(Setup.path, Setup.public)));
 app.get(Setup.request, (request, response) => {
